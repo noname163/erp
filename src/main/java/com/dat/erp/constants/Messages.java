9c21938b24458955a8b5d87d3881852c8c91bd2e
noname163 <danghuudat163@gmail.com>
2026-01-12 20:45:21 +0700

    MGR: Add new entity - Add Email entity

--- src/main/java/com/dat/erp/constants/Messages.java
+++ src/main/java/com/dat/erp/constants/Messages.java
@@ -27,5 +27,6 @@ public final class Messages {
 
     // Conflict messages
     public static final String ERROR_COMPANY_NAME_EXISTS = "Company name already exists";
+    public static final String ERROR_COMPANY_EMAIL_EXISTS = "Company email already exists";
     public static final String ERROR_DEPARTMENT_NAME_EXISTS = "Department name already exists";
 }
